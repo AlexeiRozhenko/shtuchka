af5c6fdf1b6e3faf4bcbcc5ad098e1f84ed74a5d
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-09-09 20:19:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,9 @@ if prompt := st.chat_input("Напишите ваш вопрос"):
 
     message = ChatMessage(role="assistant", content="")
     st.session_state["messages"].append(message)
+
+    with st.chat_message("assistant"):
+        with st.spinner("Обрабатываю ваш запрос..."):
+            message_placeholder = st.empty()
+                message_placeholder.markdown("Няняня")
+            message_placeholder.markdown(message.content)
