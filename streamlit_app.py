24ee567f65625f952d567a3aff3cc6988ce9255d
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-09-09 19:58:19 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.markdown("nyanyanya")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        ChatMessage(role="assistant", content="Привет! Я X6, ваш личный ассистент, который знает (почти) все-все о внутрикорпоративных тонкостях. Подскажите, чем я могу быть полезен?"),
+    ]
+
+for message in st.session_state["messages"][1::1]:
+    with st.chat_message(message.role):
+        st.markdown(message.content)
+
+if prompt := st.chat_input("Напишите ваш вопрос"):
+    message = ChatMessage(role="user", content=prompt)
+    # st.session_state["messages"].append(message)
