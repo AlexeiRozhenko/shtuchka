39cc8de7f839905eba89d2f938d6bf147755d9a3
Alexei Rozhenko <136263079+AlexeiRozhenko@users.noreply.github.com>
2024-09-12 10:38:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 from langchain.schema import ChatMessage
 
-url = "http://95.182.121.46:8080/query?query=%D0%BA%D0%B0%D0%BA%20%D1%81%D0%B4%D0%B5%D0%BB%D0%B0%D1%82%D1%8C%20%D0%B7%D0%B0%D1%8F%D0%B2%D0%BA%D1%83%20%D0%B2%20%D0%BB%D0%B8%D1%87%D0%BD%D0%BE%D0%BC%20%D0%BA%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82%D0%B5"
+url = "http://95.182.121.46:8080/docs#/default/proxy_query_query_get"
 st.set_page_config(page_title="X6 Bot", page_icon="🔍")
 
 # Отправление запроса на FastAPI сервер
